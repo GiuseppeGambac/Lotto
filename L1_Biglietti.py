3af3c0ce1318c07cc8b0aa95c7aafdb4e82246fd
giuseppe <gambacorta.giuseppe4@gmail.com>
2021-08-31 13:21:27 +0200

    ultima modifica

--- L1_Biglietti.py
+++ L1_Biglietti.py
@@ -1,10 +1,23 @@
 import random
 
+def gameNumber(typegame):
+    "Select the minimum value for let play the game"
+    if typegame == "ambata":
+        return 1
+    elif typegame =="ambo":
+        return 2
+    elif typegame == "terno":
+        return 3
+    elif typegame == "quaterna":
+        return 4
+    elif typegame == "cinquina":
+        return 5
 
 class Ticket():
     
-    def __init__(self,numberquanity,gametype : str, city):
+    def __init__(self,numberquanity,money,gametype : str, city):
         self.numberquantity = numberquanity
+        self.money = money
         self.gametype = gametype
         self.city = city
         
@@ -27,7 +40,7 @@ class Ticket():
         elif self.gametype =="ambo":
             winningnumbers = random.sample(self.numberlist,2)
             return winningnumbers
-        elif self.gametype == "terna":
+        elif self.gametype == "terno":
             winningnumbers = random.sample(self.numberlist,3)
             return winningnumbers
         elif self.gametype == "quaterna":
@@ -36,6 +49,86 @@ class Ticket():
         elif self.gametype == "cinquina":
             winningnumbers = random.sample(self.numberlist,5)
             return winningnumbers
+        
+        
+    def WinningMoney(self):
+        moneyambata   = 0.0
+        moneyambo     = 0.0
+        moneyterno    = 0.0
+        moneyquaterna  =  0.0
+        moneycinquina =  0.0
+        
+        
+        if self.numberquantity ==1:
+           moneyambata = 11,23
+        elif self.numberquantity==2:
+            moneyambata =5.61
+            moneyambo = 250
+        elif self.numberquantity ==3:
+          moneyambata   = 3.74	
+          moneyambo     = 83.33
+          moneyterno    = 4500
+        elif self.numberquantity ==4:
+          moneyambata   = 2.80	
+          moneyambo     = 41.66	
+          moneyterno    = 1125
+          moneyquaterna  =  12000	  
+        elif self.numberquantity ==5:
+          moneyambata   = 2.24
+          moneyambo     = 25	
+          moneyterno    = 450
+          moneyquaterna  =  24000	
+          moneycinquina =  6000000
+        elif self.numberquantity ==6:
+          moneyambata   = 1.87	
+          moneyambo     = 16.66
+          moneyterno    = 225
+          moneyquaterna  =  8000	
+          moneycinquina =  1000000
+        elif self.numberquantity ==7:
+          moneyambata   = 1.60	
+          moneyambo     = 11.90	
+          moneyterno    = 128.57	
+          moneyquaterna  =  2428.57	
+          moneycinquina =  285714.28
+        elif self.numberquantity ==8:
+          moneyambata   = 1.40	
+          moneyambo     = 8.92	
+          moneyterno    = 80.35
+          moneyquaterna  =  1714.28
+          moneycinquina =  107142.85 
+        elif self.numberquantity ==9:
+          moneyambata   = 1.24	
+          moneyambo     = 6.94	
+          moneyterno    = 53.57
+          moneyquaterna  =  952.38	
+          moneycinquina =  47619.04
+        elif self.numberquantity ==10:
+          moneyambata   = 1.12	
+          moneyambo     = 5.55	
+          moneyterno    = 37.50	
+          moneyquaterna  =  571,42	
+          moneycinquina =  23.809,52
+        
+        
+        if self.gametype == "ambata":
+          total= self.money * moneyambata
+        elif self.gametype == "ambo":
+           total=  self.money * moneyambo
+        elif self.gametype == "terno":
+          total = self.money * moneyterno
+        elif self.gametype == "quaterna":
+          total = self.money * moneyquaterna
+        elif self.gametype == "cinquina":
+          total= self.money * moneycinquina
+           
+        if total > 500:
+            withtax = total *0.92
+            return total,withtax
+        else:
+            return total
+        
+       
     
     
     def printTicket(self):
@@ -51,6 +144,7 @@ def ticketsGeneration():
     #list of tickets
     tickets = [] 
      
+    # tickets generation
     while True:
      try:
         ticketsnumber = int(input(("quanti biglietti vuoi generare? ")))
@@ -63,29 +157,45 @@ def ticketsGeneration():
          
      except:
         print("inserisci un numero")
-        
+    
+    # for every tickets generated    
     for i in range(ticketsnumber):
      input_number = 0
+     money = 0
      input_game =""
      input_city = ""
+     
+     # select the type of game
+     while True:
+      print("seleziona un tipo di puntata fra quelli elencati")
+      print(type_of_game)    
+      input_game = input()
+      if input_game in type_of_game:
+         break
+      else:
+         print('valore non presente')
+         input_game ==""
+         
+     # minumum range for the type of game
+     minimum = gameNumber(input_game)
+     
      #number selection
-     while input_number <= 4 or input_number > 10:   
+     while input_number < minimum or input_number > 10:   
       try:
-       input_number = int(input('inserisci un numero fra 5 e 10:'))
+       input_number = int(input('inserisci un numero fra %d e 10:' % minimum))
       except:
           print("devi inserire un numero")
-          
-    
-     while True:
-          print("seleziona un tipo di puntata fra quelli elencati")
-          print(type_of_game)    
-          input_game = input()
-          if input_game in type_of_game:
-              break
-          else:
-              print('valore non presente')
-              input_game ==""
-              
+       
+     #money selection
+     while money == 0:   
+           try:
+            money = int(input('inserisci la scommessa:'))
+           except:
+               print("devi inserire un numero")
+      
+     
+     
+     # city selection
      while True:
       print("seleziona una ruota fra quelle elencate")
       print(city)    
@@ -96,8 +206,8 @@ def ticketsGeneration():
           print('valore non presente')
           input_city ==""
  
- 
-     tickets.append(Ticket(input_number,input_game,input_city))
+     # add the ticket at the list
+     tickets.append(Ticket(input_number,money,input_game,input_city))
  
      
     return tickets
@@ -109,7 +219,7 @@ if __name__ == '__main__':
  
  for i in tickets:
      i.printTicket()
-
+     print(i.winningnumbers)
 
    
 
